507516185a721901fc2f3f257eeec30c5c18c3ef
habib24457 <habiburehman390@gmail.com>
2025-10-22 15:45:38 +0200

    Mig

---
+++ BistroFoodReview.Api/Migrations/20251022132215_ChangeStarsToDouble.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace BistroFoodReview.Api.Migrations
+{
+    /// <inheritdoc />
+    public partial class ChangeStarsToDouble : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<double>(
+                name: "Stars",
+                table: "Ratings",
+                type: "double precision",
+                nullable: false,
+                oldClrType: typeof(int),
+                oldType: "integer");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<int>(
+                name: "Stars",
+                table: "Ratings",
+                type: "integer",
+                nullable: false,
+                oldClrType: typeof(double),
+                oldType: "double precision");
+        }
+    }
+}
