e91dda8a1ce11c09184984021de49a1018ea662d
habib24457 <habiburehman390@gmail.com>
2025-10-26 15:42:13 +0100

    New tests for Rating controller

--- BistroFoodReview.Api.Test/Controllers/RatingControllerTests.cs
+++ BistroFoodReview.Api.Test/Controllers/RatingControllerTests.cs
@@ -89,4 +89,153 @@ public class RatingControllerTests
         Assert.NotNull(ratingInDb);
         Assert.Equal(5, ratingInDb.Stars);
     }
+
+    [Fact]
+    public async Task SaveRating_ShouldReturnNull_WhenRatingIsNotBetween1To5()
+    {
+        // Arrange
+        var options = new DbContextOptionsBuilder<BistroReviewDbContext>()
+        .UseInMemoryDatabase(databaseName: Guid.NewGuid().ToString())
+        .Options;
+
+        await using var context = new BistroReviewDbContext(options);
+
+        var mealOption = new MealOption { Id = Guid.NewGuid(), Name = "Grill Sandwiches" };
+        context.MealOptions.Add(mealOption);
+
+        var meal = new Meal
+            { 
+                Id = Guid.NewGuid(),
+                Date = DateTime.UtcNow,
+                MealOptionId = mealOption.Id,
+                MealOption = mealOption,
+                EditedMealName = "Test Meal",
+                Ratings = new List<Rating>()
+            };
+        context.Meals.Add(meal);
+
+        var user = new User { Id = Guid.NewGuid(), FirstName = "Test", LastName = "User" };
+        context.Users.Add(user);
+
+        await context.SaveChangesAsync();
+
+        var mealRepo = new MealRepository(context);
+        var ratingRepo = new RatingRepository(context);
+        var logger = Substitute.For<ILogger<RatingController>>();
+
+        // Mock mapper
+        var mapper = Substitute.For<IMapper>();
+        var createRatingDto = new CreateRatingDto
+        {
+            MealId = meal.Id,
+            UserId = user.Id,
+            Stars = 6
+        };
+
+        var ratingEntity = new Rating
+        {
+            Id = Guid.NewGuid(),
+            MealId = meal.Id,
+            UserId = user.Id,
+            Stars = createRatingDto.Stars,
+            Meal = meal,
+            User = user
+        };
+
+        var ratingDto = new RatingDto
+        {
+            Id = ratingEntity.Id,
+            MealId = meal.Id,
+            Stars = createRatingDto.Stars,
+            MealDate = meal.Date,
+            EditedMealName = meal.EditedMealName,
+            MealOptionName = meal.MealOption.Name
+        };
+
+        mapper.Map<Rating>(createRatingDto).Returns(ratingEntity);
+        mapper.Map<RatingDto>(Arg.Any<Rating>()).Returns(ratingDto);
+
+        var controller = new RatingController(ratingRepo, mealRepo, mapper, logger);
+
+        // Act
+        controller.SaveRating(createRatingDto);
+
+        // Assert
+        var ratingInDb = await context.Ratings.FirstOrDefaultAsync(r => r.MealId == meal.Id && r.UserId == user.Id);
+        Assert.Null(ratingInDb);
+    }
+    
+      [Fact]
+    public async Task SaveRating_ShouldReturnNull_WhenDateIsNotTodaysDate()
+    {
+        // Arrange
+        var options = new DbContextOptionsBuilder<BistroReviewDbContext>()
+        .UseInMemoryDatabase(databaseName: Guid.NewGuid().ToString())
+        .Options;
+
+        await using var context = new BistroReviewDbContext(options);
+
+        var mealOption = new MealOption { Id = Guid.NewGuid(), Name = "Grill Sandwiches" };
+        context.MealOptions.Add(mealOption);
+
+        var meal = new Meal
+            { 
+                Id = Guid.NewGuid(),
+                Date = DateTime.UtcNow.AddDays(2),                MealOptionId = mealOption.Id,
+                MealOption = mealOption,
+                EditedMealName = "Test Meal",
+                Ratings = new List<Rating>()
+            };
+        context.Meals.Add(meal);
+
+        var user = new User { Id = Guid.NewGuid(), FirstName = "Test", LastName = "User" };
+        context.Users.Add(user);
+
+        await context.SaveChangesAsync();
+
+        var mealRepo = new MealRepository(context);
+        var ratingRepo = new RatingRepository(context);
+        var logger = Substitute.For<ILogger<RatingController>>();
+
+        // Mock mapper
+        var mapper = Substitute.For<IMapper>();
+        var createRatingDto = new CreateRatingDto
+        {
+            MealId = meal.Id,
+            UserId = user.Id,
+            Stars = 5
+        };
+
+        var ratingEntity = new Rating
+        {
+            Id = Guid.NewGuid(),
+            MealId = meal.Id,
+            UserId = user.Id,
+            Stars = createRatingDto.Stars,
+            Meal = meal,
+            User = user
+        };
+
+        var ratingDto = new RatingDto
+        {
+            Id = ratingEntity.Id,
+            MealId = meal.Id,
+            Stars = createRatingDto.Stars,
+            MealDate = meal.Date,
+            EditedMealName = meal.EditedMealName,
+            MealOptionName = meal.MealOption.Name
+        };
+
+        mapper.Map<Rating>(createRatingDto).Returns(ratingEntity);
+        mapper.Map<RatingDto>(Arg.Any<Rating>()).Returns(ratingDto);
+
+        var controller = new RatingController(ratingRepo, mealRepo, mapper, logger);
+
+        // Act
+        controller.SaveRating(createRatingDto);
+
+        // Assert
+        var ratingInDb = await context.Ratings.FirstOrDefaultAsync(r => r.MealId == meal.Id && r.UserId == user.Id);
+        Assert.Null(ratingInDb);
+    }
 }
